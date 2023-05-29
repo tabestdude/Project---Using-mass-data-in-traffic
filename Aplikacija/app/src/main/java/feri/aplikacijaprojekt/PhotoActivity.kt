1526fd9dcd5af33378441d72b4343bf06025f0fd
Majer <luka.majer2000@gmail.com>
2023-05-29 18:16:12 +0200

    CAMERA WORKS
    CAMERA WORKS

--- Aplikacija/app/src/main/java/feri/aplikacijaprojekt/PhotoActivity.kt
+++ Aplikacija/app/src/main/java/feri/aplikacijaprojekt/PhotoActivity.kt
@@ -53,7 +53,7 @@ class PhotoActivity : AppCompatActivity() {
         // Request camera permission
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             // Permission has already been granted
-            //startCamera()
+            startCamera()
         } else {
             // Permission has not yet been granted, request it
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
@@ -62,7 +62,7 @@ class PhotoActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        //startCamera()
+        startCamera()
     }
 
     override fun onPause() {
@@ -71,37 +71,31 @@ class PhotoActivity : AppCompatActivity() {
     }
 
     private fun startCamera() {
-        // Create a preview use case
-        val preview = Preview.Builder()
-            .setTargetAspectRatio(AspectRatio.RATIO_4_3)
-            .build()
-
-        // Create an image capture use case
-        imageCapture = ImageCapture.Builder()
-            .setTargetResolution(Size(480, 640))
-            .build()
-
-        // Select the front-facing camera
-        val cameraSelector = CameraSelector.Builder()
-            .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
-            .build()
-
-        // Bind the camera use cases to the viewfinder
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraProviderFuture.addListener({
             val cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
             try {
-                // Unbind any previous use cases before rebinding
                 cameraProvider.unbindAll()
-
-                // Bind the new use cases to the camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture)
-
-                // Connect the preview to the viewfinder
-                preview.setSurfaceProvider(viewFinder.surfaceProvider)
-            } catch (e: Exception) {
-                Log.e(TAG, "Failed to bind camera use cases", e)
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+            } catch (exc: Exception) {
+                Log.e(TAG, "Unable to start camera", exc)
             }
         }, ContextCompat.getMainExecutor(this))
     }
@@ -111,42 +105,38 @@ class PhotoActivity : AppCompatActivity() {
     }
 
     private fun takePhoto() {
-        // Create a file to store the photo
-        val photoFile = File(externalMediaDirs.first(), "${System.currentTimeMillis()}.jpg")
+        val imageCapture = imageCapture ?: return
 
-        // Create an image capture listener
-        val imageCaptureListener = object : ImageCapture.OnImageSavedCallback {
-            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                // Save the captured image to the file
-                val savedUri = outputFileResults.savedUri ?: Uri.fromFile(photoFile)
-                val msg = "Photo capture succeeded: $savedUri"
-                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        val photoFile = File(externalMediaDirs.firstOrNull(), "${System.currentTimeMillis()}.jpg")
 
-                // Hide the camera preview and show the continue button
-                viewFinder.visibility = View.GONE
-                btnTakePhoto.visibility = View.GONE
-                btnContinue.visibility = View.VISIBLE
-            }
-
-            override fun onError(exception: ImageCaptureException) {
-                // Handle capture failure
-                Log.e(TAG, "Capture failed: ${exception.message}", exception)
-            }
-        }
-
-        // Configure the image capture use case to save the photo to the file
         val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
-        imageCapture?.takePicture(outputOptions, cameraExecutor, imageCaptureListener)
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo capture succeeded: $savedUri"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+
+                    viewFinder.visibility = View.GONE
+                    btnTakePhoto.visibility = View.GONE
+                    btnContinue.visibility = View.VISIBLE
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exception.message}", exception)
+                }
+            })
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CAMERA_PERMISSION) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission has been granted, start the camera
                 startCamera()
             } else {
-                // Permission has been denied, show an error message
                 Toast.makeText(this, "Camera permission is required to take photos", Toast.LENGTH_SHORT).show()
                 finish()
             }
